33ea42cbde20d4f524673ec046288b83b3a5ee69
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-25 18:03:19 +0530

    Update GitGram.py

--- GitGram.py
+++ GitGram.py
@@ -38,7 +38,7 @@ def start(_bot, update):
     """/start message for bot"""
     message = update.effective_message
     message.reply_text(
-        f"This is the Updates watcher for {PROJECT_NAME}. I am just notify users about what's happen on their Git repositories thru webhooks.\n\nYou need to [Mizuhara-Repo](https://github.com/AnuragSharma080/MizuharaSmexyBot) or see /help to use this bot on your groups.",
+        f"This is the Updates watcher for {PROJECT_NAME}. I am just notify users about what's happen on their Git repositories thru webhooks.\n\nYou need to [self-host](https://waa.ai/GitGram) or see /help to use this bot on your groups.",
         parse_mode="markdown")
 
 
