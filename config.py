7b51b253842e6fd88a458a72134adfa3bde75ea7
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-08-30 05:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
 PROJECT_NAME = "This is the Updates watcher for Direct Updates Of @SiderzBot From Github TO Telegram"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/DarkCybers/Innexiabot"
+GIT_REPO_URL = "https://github.com/DarkCybers/Innexia"
